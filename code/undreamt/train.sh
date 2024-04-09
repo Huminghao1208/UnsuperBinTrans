354471447ce218cfec63aa095f20fa6d8f299f79
Huminghao1208 <mhu20@gmu.edu>
2024-04-09 19:52:44 -0400

    new

--- code/undreamt/train.sh
+++ code/undreamt/train.sh
@@ -7,7 +7,7 @@ x86_CORPUS_DIR=$DATA_PATH/x86_corpus
 arm32_EMB_DIR=$DATA_PATH/arm32_emb
 x86_EMB_DIR=$DATA_PATH/x86_emb
 
-python train.py --src $arm32_CORPUS_DIR/arm32 --trg $x86_CORPUS_DIR/x86 \
+python train.py --src $arm32_CORPUS_DIR/train.arm32 --trg $x86_CORPUS_DIR/train.x86 \
 --src_embeddings $arm32_EMB_DIR/arm32.emb --trg_embeddings $x86_EMB_DIR/x86.emb \
---save model/arm32_to_x86 --save_interval 100 --log_interval 100 \
- --batch 256 --cuda
+--save model/arm32_to_x86 --save_interval 2000 --log_interval 100 \
+ --batch 512 --cuda
