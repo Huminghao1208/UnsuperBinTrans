a604a28dba3c23998a54308da66323c011081d2d
Huminghao1208 <mhu20@gmu.edu>
2024-04-10 19:14:08 -0400

    a new version

--- code/undreamt/translate_batch.sh
+++ code/undreamt/translate_batch.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
 # Translate files in the "arm32_benign" folder
-for file in data/Malware_detection/arm32_benign/*; do
-    if [ -f "$file" ]; then
-        filename=$(basename "$file")
-        python translate.py model/arm32_to_x86.it24000.src2trg.pth \
-            --input "$file" \
-            --output "data/Malware_detection/arm32_to_x86_benign/$filename"
-    fi
-done
+# for file in data/Malware_detection/arm32_benign/*; do
+#     if [ -f "$file" ]; then
+#         filename=$(basename "$file")
+#         python translate.py model/arm32_to_x86.it24000.src2trg.pth \
+#             --input "$file" \
+#             --output "data/Malware_detection/arm32_to_x86_benign/$filename"
+#     fi
+# done
 
-Translate files in the "arm32_malware" folder
-for file in data/Malware_detection/arm32_malware/*; do
+# Translate files in the "arm32_malware" folder
+# for file in data/Malware_detection/arm32_malware/*; do
+#     if [ -f "$file" ]; then
+#         filename=$(basename "$file")
+#         python translate.py model/arm32_to_x86.it24000.src2trg.pth \
+#             --input "$file" \
+#             --output "data/Malware_detection/arm32_to_x86_malware/$filename"
+#     fi
+# done
+
+
+for file in data/Evaluation/arm32_diffutils/*; do
     if [ -f "$file" ]; then
         filename=$(basename "$file")
         python translate.py model/arm32_to_x86.it24000.src2trg.pth \
             --input "$file" \
-            --output "data/Malware_detection/arm32_to_x86_malware/$filename"
+            --output "data/Evaluation/x86_diffutils_translated/$filename"
     fi
 done
